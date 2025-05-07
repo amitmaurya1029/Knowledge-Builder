9df469c8945aa45a784eba489c25e2f7e75986c0
AMIT <rs.mauryarocks@gmail.com>
2025-05-07 20:16:23 +0530

    Made Cube block ui

--- Assets/Scripts/CubeChecker.cs
+++ Assets/Scripts/CubeChecker.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections.Generic;
+using OpenCover.Framework.Model;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class CubeChecker : MonoBehaviour
 {
+    public static event EventHandler<CubeCheckerEventArgs> OnCubeAdded;
     public Vector3 boxCenter = Vector3.zero;
     public Vector3 boxSize = new Vector3(2.34f, 0.08f, 1f);
     public LayerMask detectionLayer;
 
     private int CubeBlockCount = 0;
+    
+    
     private List<GameObject> cubes = new List<GameObject>();
 
 
@@ -22,9 +26,24 @@ public class CubeChecker : MonoBehaviour
             if (hit.gameObject.tag == "CubeBlock" && !cubes.Contains(hit.gameObject))
             {
                 AddingCube(hit.gameObject);   
+                OnCubeAdded?.Invoke(this, new CubeCheckerEventArgs(this));
             }
         }
-        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        foreach (GameObject cube in cubes)
+        {
+            if (cube == collision.gameObject)
+            {
+                Debug.Log("Remove object : " + cube.name + "cubes count :" + cubes.Count);
+                cubes.Remove(collision.gameObject);
+                Debug.Log("Remove object : " + cube.name + "cubes count :" + cubes.Count);
+                OnCubeAdded?.Invoke(this, new CubeCheckerEventArgs(this));
+
+            }
+        }
     }
 
 
@@ -35,9 +54,28 @@ public class CubeChecker : MonoBehaviour
          Debug.Log(" Total numbers of cubes :" + CubeBlockCount );
     }
     
+    public int GetCubeCount()
+    {
+        return CubeBlockCount;
+    }
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireCube(transform.position + boxCenter, boxSize);
     }
+
+     
 }
+
+public class CubeCheckerEventArgs : EventArgs
+{
+    public CubeChecker cubeChecker {get;}
+    public bool canUpdateUi = true;
+    public CubeCheckerEventArgs(CubeChecker cubeChecker)
+    {
+        this.cubeChecker = cubeChecker;
+    } 
+
+}
+
--- Assets/Scripts/CubeCheckerUi.cs
+++ Assets/Scripts/CubeCheckerUi.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,16 +7,32 @@ using UnityEngine;
 public class CubeCheckerUi : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI text;
-
+    private bool canUpdateUi = true;
+    private int maxCubeCount = 5;
+    
     void Start()
     {
-    //    CubeChecker.on
-
+        CubeChecker.OnCubeAdded += UpdateUi;
     }
 
-    private void UpdateUi()
+    private void UpdateUi(object sender, CubeCheckerEventArgs e)
     {
+        Debug.Log("value cube : " + e.cubeChecker.GetCubeCount());
         
+        if (!canUpdateUi) {return;}
+
+        if(e.cubeChecker.GetCubeCount() == maxCubeCount)
+        {
+            text.text = $"You have Added all {e.cubeChecker.GetCubeCount()} cubes";
+            canUpdateUi = false;
+        }
+
+        else
+        {
+            text.text = $"Current Added cube {e.cubeChecker.GetCubeCount()}/{5}";
+        }
     }
+
+    
 
 }
