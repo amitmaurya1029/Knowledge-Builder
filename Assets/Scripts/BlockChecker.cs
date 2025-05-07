2e9aa500ddb8c551261ca29a10675c143c73eb6a
AMIT <rs.mauryarocks@gmail.com>
2025-05-07 16:15:31 +0530

    just commit

--- Assets/Scripts/BlockChecker.cs
+++ Assets/Scripts/BlockChecker.cs
@@ -1,21 +1,39 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class BlockChecker : MonoBehaviour
+public class CubeChecker : MonoBehaviour
 {
     public Vector3 boxCenter = Vector3.zero;
     public Vector3 boxSize = new Vector3(2.34f, 0.08f, 1f);
     public LayerMask detectionLayer;
 
-    void Update()
+    private int CubeBlockCount = 0;
+    private List<GameObject> cubes = new List<GameObject>();
+
+
+    private void OnCollisionEnter(Collision collision)
     {
         Collider[] hits = Physics.OverlapBox(transform.position + boxCenter, boxSize, Quaternion.identity, detectionLayer);
 
         foreach (var hit in hits)
         {
-            Debug.Log("Detected object: " + hit.name);
+            if (hit.gameObject.tag == "CubeBlock" && !cubes.Contains(hit.gameObject))
+            {
+                AddingCube(hit.gameObject);   
+            }
         }
+        
     }
 
+
+    private void AddingCube(GameObject cube)
+    {
+         cubes.Add(cube);
+         CubeBlockCount++;
+         Debug.Log(" Total numbers of cubes :" + CubeBlockCount );
+    }
     
     private void OnDrawGizmosSelected()
     {
---
+++ Assets/Scripts/CubeCheckerUi.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CubeCheckerUi : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+
+    void Start()
+    {
+    //    CubeChecker.on
+
+    }
+
+    private void UpdateUi()
+    {
+        
+    }
+
+}
