70a2c4448536ae0ecfa91a92bb73a0a35fa5f016
AMIT <rs.mauryarocks@gmail.com>
2025-05-08 08:48:04 +0530

    working on level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject[] levels;
+    private int currentLevelIndex = 0;
+
+    void Start()
+    {
+        LoadLevel(currentLevelIndex);
+    }
+
+    public void LoadLevel(int index)
+    {
+        // Disable all levels first
+        for (int i = 0; i < levels.Length; i++)
+        {
+            levels[i].SetActive(false);
+        }
+
+        // Enable selected level
+        if (index >= 0 && index < levels.Length)
+        {
+            levels[index].SetActive(true);
+            currentLevelIndex = index;
+        }
+    }
+
+    public void NextLevel()
+    {
+        int nextLevel = currentLevelIndex + 1;
+        if (nextLevel < levels.Length)
+        {
+            LoadLevel(nextLevel);
+        }
+        else
+        {
+            Debug.Log("All levels completed!");
+        }
+    }
+
+    public void RestartLevel()
+    {
+        LoadLevel(currentLevelIndex);
+    }
+}
